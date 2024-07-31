9024b6ea9bba76a768cb6e4e323f43f626ab797e
Ксения Че <chechulina75@list.ru>
2024-07-31 20:16:21 +0500

    Add files via upload

--- module_3_3.py
+++ module_3_3.py
@@ -1,29 +1,15 @@
-# 1
-def print_params(a, b, c):
+def print_params(a=1, b='строка', c=True):
     print(a, b, c)
 
 
-def print_params(*args):
-    print(args)
-
-
-list_ = [1, 'строка', True]
-print_params(list_, 25, [123])
-print_params(10, 'l', list_)
-print_params(2, True, False)
-print_params(*list_)
+print_params(b=25)
+print_params(c=[1, 2, 3])
 print_params()
 
-
 # 2
-def print_params(a, b, c):
-    print(a, b, c)
-
-
 values_list = ['apple', 3.14, (1, 2, 3)]
 values_dict = {'a': 3.14, 'b': 'apple', 'c': (1, 2, 3)}
 print_params(*values_list)
-
 print_params(**values_dict)
 
 # 3
