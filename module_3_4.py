9d433d20fb5d2b5ab68952d9287b17fc9c91fefa
Ксения Че <chechulina75@list.ru>
2024-08-03 15:51:19 +0500

    Add files via upload

---
+++ module_3_4.py
@@ -0,0 +1,9 @@
+def single_root_words(root_word, *other_words):
+    same_words = ()
+    same_words = (other_words)
+    for same_words in other_words:
+        if same_words == 'rich':
+            print(result1)
+            print(result2)
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
